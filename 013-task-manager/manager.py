df986111e3e499e41c4f9a4490442ab7cbb16e5c
admin <tazhehyad@gmail.com>
2026-07-31 18:25:51 +0330

    added proj013 - version1

---
+++ 013-task-manager/manager.py
@@ -0,0 +1,219 @@
+from enum import Enum
+from uuid import uuid7
+from datetime import datetime
+
+
+class User:
+
+    class Roles(Enum):
+        ADMIN = 'admin'
+        USER = 'user'
+        MANAGER = 'manager'
+
+    def __init__(self, name, user_id, email):
+        self.name = name
+        self.user_id = user_id # usually its ssn so better not use uuid
+        self.email = email
+        self.role = User.Roles.USER
+        self.tasks = []
+
+    def __str__(self):
+        return f'{self.name} | {self.user_id} | {self.email} | {self.role.value}'
+
+class Task:
+    class Status(Enum):
+        PENDING = 'pending'
+        TODO = 'todo'
+        IN_PROGRESS = 'in_progress'
+        DONE = 'done'
+        CANCELED = 'canceled'
+
+    class Priority(Enum):
+        LOW = 'low'
+        MEDIUM = 'medium'
+        HIGH = 'high'
+
+    def __init__(self, title, description,priority, due_date):
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.created_at = datetime.now()
+        self.task_id = str(uuid7())
+        self.assigned_user = None
+        self.status = Task.Status.TODO
+        if not isinstance(priority, Task.Priority):
+            raise TypeError("Priority must be of type Task.Priority")
+        self.priority = priority
+
+    def __str__(self):
+        assigned = self.assigned_user.name if self.assigned_user else "Unassigned"
+        return (
+            f"task: {self.title}, "
+            f"assigned to: {assigned}, "
+            f"priority: {self.priority.value}, "
+            f"created_at: {self.created_at}, "
+            f"due_date: {self.due_date}, "
+            f"status: {self.status.value}, "
+
+        )
+
+
+
+class Project:
+    def __init__(self, name, project_id):
+        self.name = name
+        self.project_id = project_id
+        self.tasks = []
+        self.users = []
+
+    def add_user(self, user):
+        if user is None:
+            raise ValueError("User cannot be None")
+        if user in self.users:
+            raise ValueError("User already exists")
+        self.users.append(user)
+        return user
+
+    def add_task(self, task):
+        if task is None:
+            raise ValueError("Task cannot be None")
+        if task in self.tasks:
+            raise ValueError("Task already exists")
+        self.tasks.append(task)
+        return task
+
+    def find_task(self, task_id):
+        for task in self.tasks:
+            if task.task_id == task_id:
+                return task
+        return None
+
+    def find_user(self, user_id):
+        for user in self.users:
+            if user.user_id == user_id:
+                return user
+        return None
+
+    # find and check user and the task both exists
+    # if task assigned to a user then remove task from previous user's tasks list
+    # assign the new user to the task
+    # assign task to the new user's tasks list (if it's not alr there)
+    # return the task
+    def assign_task(self, task_id, user_id):
+        task = self.find_task(task_id)
+        if task is None:
+            raise ValueError("Task does not exist")
+
+        user = self.find_user(user_id)
+        if user is None:
+            raise ValueError("User does not exist")
+
+        if task.assigned_user is not None:
+            task.assigned_user.tasks.remove(task)
+        task.assigned_user = user
+
+        if task not in user.tasks:
+            user.tasks.append(task)
+        return task
+
+    # find and check the task exists
+    # if task is assigned to a user then remove the task from the user's tasks list
+    # remove the task from the project's tasks list
+    # return task
+    def remove_task(self, task_id):
+        task = self.find_task(task_id)
+        if task is None:
+            raise ValueError("Task does not exist")
+
+        if task.assigned_user is not None:
+            task.assigned_user.tasks.remove(task)
+
+        self.tasks.remove(task)
+        return task
+
+    # find and check if the user exists
+    # for each task assigned to the user
+    # unassign the task
+    # clear the user's task list
+    # remove the user from the project's users list
+    # return the user
+    def remove_user(self, user_id):
+        user = self.find_user(user_id)
+        if user is None:
+            raise ValueError("User does not exist")
+
+        for task in user.tasks:
+            task.assigned_user = None
+
+        user.tasks.clear()
+        self.users.remove(user)
+        return user
+
+    # find and check if the task exists
+    # validate the new status
+    # change the task.status value into the given value
+    # return the task
+    def change_task_status(self, task_id, status):
+        task = self.find_task(task_id)
+        if task is None:
+            raise ValueError("Task does not exist")
+        if not isinstance(status, Task.Status):
+            raise TypeError("Status must be of type Task.Status")
+        task.status = status
+        return task
+
+    # for each task in project's tasks list
+    # print the task.__str__
+    def show_tasks(self):
+        for task in self.tasks:
+            print(task)
+
+    def show_users(self):
+        for user in self.users:
+            print(user)
+
+    # find and check if the user exists
+    # loop through users tasks
+    # show each task
+    def show_users_tasks(self, user_id):
+        user = self.find_user(user_id)
+        if user is None:
+            raise ValueError("User does not exist")
+        for task in user.tasks:
+            print(task)
+
+    def change_user_role(self, user_id, role):
+        user = self.find_user(user_id)
+        if user is None:
+            raise ValueError("User does not exist")
+        if not isinstance(role, User.Roles):
+            raise TypeError("Role must be of type User.Roles")
+        user.role = role
+        return user
+
+
+    # show project name
+    # show how many users working on the proj
+    # show how many tasks the project have
+    # show how many tasks are done
+    # show how many tasks are in pending mode
+    def project_summary(self):
+        # also can use a list comprehension with a filter() and a lambda status in the filter
+        done_tasks = [task for task in self.tasks if task.status == Task.Status.DONE]
+        pending_tasks = [task for task in self.tasks if task.status == Task.Status.PENDING]
+        return(
+            f'project: {self.name}, '
+            f'users: {len(self.users)}, '
+            f'tasks: {len(self.tasks)}, '
+            f'done: {done_tasks}, '
+            f'pending: {pending_tasks}'
+        )
+
+
+
+
+
+
+
+
+
